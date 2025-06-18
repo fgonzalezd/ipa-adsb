b73e528520708681bbe7a4981c8b3ad8008e5e1e
fernando <56043800+fgonzalezd@users.noreply.github.com>
2025-06-18 02:09:28 -0600

    Update avrdecoder.py
    updated file

--- avrdecoder.py
+++ avrdecoder.py
@@ -1,10 +1,12 @@
-# This script takes an avro formatted file and extracts the ADSB raw messages
-# to a text file.
-# The avro is obtained from https://opensky-network.org/datasets/adsb
+"""
+This script takes an avro formatted file and extracts the ADSB raw messages
+to a text file.
+The avro is obtained from https://opensky-network.org/datasets/adsb
 
-# Author        : Fernando Gonzalez
-# Created on    : 6/17/2025
-# Last update   : 6/17/2025
+Author        : Fernando Gonzalez
+Created on    : 6/17/2025
+Last update   : 6/17/2025
+"""
 
 import pyModeS as pms
 from fastavro import reader
@@ -16,7 +18,7 @@ with open(avro_scr_file, 'rb') as f, open(raw_data, 'w') as out_file:
     avro_reader = reader(f)
     for record in avro_reader:
         msg = record["rawMessage"]
-        if msg:
-            out_file.write(msg + "\n")
+        if msg:# and pms.icao(msg) == "4008f9":
+            out_file.write(msg + "\tICAO: " + pms.icao(msg) + "\tType Code: " + str(pms.adsb.typecode(msg)) + "\n")
         #if pms.df(msg) == 17:
         #    print("ICAO:", pms.icao(msg), "Type Code:", pms.adsb.typecode(msg))
