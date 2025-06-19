64bd8bf121fbe2e4f319af3c75fa3c9c325f7295
fernando <56043800+fgonzalezd@users.noreply.github.com>
2025-06-19 01:14:57 -0600

    Fixed issue when a client disconnects

--- Tests/Server.py
+++ Tests/Server.py
@@ -9,12 +9,16 @@ def handle_client(conn, addr):
     print(f"Conectado: {addr}")
     with conn:
         while True:
-            data = conn.recv(1024)
-            if not data:
-                print(f"Cliente desconectado: {addr}")
+            try:
+                data = conn.recv(1024)
+                if not data:
+                    print(f"Cliente desconectado: {addr}")
+                    break
+                print(f"[{addr}] {data.decode()}")
+                conn.sendall(b"Mensaje recibido\n")
+            except:
                 break
-            print(f"[{addr}] {data.decode()}")
-            conn.sendall(b"Mensaje recibido\n")
+        conn.close()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
