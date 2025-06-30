7d2e131be9a7d800a81385e3cb6441429f74c1f0
fernando <56043800+fgonzalezd@users.noreply.github.com>
2025-06-29 22:23:14 -0600

    Update avrdecoder.py

--- Tests/avrdecoder.py
+++ Tests/avrdecoder.py
@@ -12,15 +12,16 @@ import pyModeS as pms
 from fastavro import reader
 
 avro_scr_file = "raw20150421_sample.avro"
-raw_data = "raw_messages.txt"
+raw_data = "raw_messages_4ca8af.txt"
+icaos = []
 
 with open(avro_scr_file, 'rb') as f, open(raw_data, 'w') as out_file:
     avro_reader = reader(f)
     for record in avro_reader:
         msg = record["rawMessage"]
-        if msg:# and pms.icao(msg) == "4008f9":
-            
+        if msg and pms.icao(msg) == "4ca8af":
+            out_file.write(msg + "\n")
             #out_file.write(msg + "\tICAO: " + pms.icao(msg) + "\tType Code: " + str(pms.adsb.typecode(msg)) + "\n")
-            out_file.write(pms.icao(msg) + "\n")
+            #out_file.write(pms.icao(msg) + "\n")
         #if pms.df(msg) == 17:
         #    print("ICAO:", pms.icao(msg), "Type Code:", pms.adsb.typecode(msg))
